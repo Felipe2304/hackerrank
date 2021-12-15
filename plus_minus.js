8168a66f2e2475b8cf0ef69c77e327fbe8df356b
Felipe martins <felipeaxandre936@gmail.com>
2021-12-15 13:27:02 -0300

    plus_minus

---
+++ plus_minus.js
@@ -0,0 +1,27 @@
+const arr = [-4,3,-9,0,4,1]
+
+const plusMinus=(arr)=>{
+  
+  let positivos = 0 
+  let negativos = 0
+  let zero = 0
+  let matriz = arr.length
+
+  for(let i = 0 ; i < matriz ; i++){
+    let numerosDaLista = arr[i]
+    
+    if(numerosDaLista < 0){
+      negativos++
+    }else if(numerosDaLista > 0){
+      positivos++
+    }else{
+      zero++
+    }
+  }
+  console.log((negativos/matriz).toFixed(6))
+  console.log((positivos/matriz).toFixed(6))
+  console.log((zero/matriz).toFixed(6))
+  
+}
+
+plusMinus(arr)

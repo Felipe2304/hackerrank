c552a7626b03516c61e6783b5b6445e2226438f3
Felipe martins <felipeaxandre936@gmail.com>
2022-01-22 20:36:16 -0300

    designer_pdf_viwer

--- designer_pdf_viwer.js
+++ designer_pdf_viwer.js
@@ -3,29 +3,23 @@ const checkResult = (a, b) => {
   else console.log("resposta errada", b, "deveria ser", a);
 };
 
-const  designerPdfViewer = (h, word)=> {
-  const alphabet = "abcdefghijklmnopqrstuvwxyz"
-  const letter = word.split('')
-  const listIndex = []
+const designerPdfViewer = (h, word) => {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const letter = word.split("");
+  const listIndex = [];
 
-  for(let i = 0 ; i < letter.length ; i++){
-    const positionLetter = alphabet.indexOf(letter[i])
-    for(let a = 0 ; a < h.length ;a++){
-     if(a === positionLetter) listIndex.push(h[a])
+  for (let i = 0; i < letter.length; i++) {
+    const positionLetter = alphabet.indexOf(letter[i]);
+    for (let a = 0; a < h.length; a++) {
+      if (a === positionLetter) listIndex.push(h[a]);
     }
   }
-  let higheNumber = listIndex[0]
-  for(let i = 0 ; i < listIndex.length ; i++){
-    if(higheNumber < listIndex[i]) higheNumber = listIndex[i]
-  }
-  
-  return higheNumber * 1 * word.length
-  
-}
-const arr = [6,5,7,3,6,7,3,4,4,2,3,7,1,3,7,4,6,1,2,4,3,3,1,1,3,5]
-const resulted = designerPdfViewer(arr,'nzbkkfhwplj')
-checkResult(70,resulted)
+  let higheNumber = Math.max(...listIndex);
 
-//[0,1,2]
-// a b c 
-// 1 3 1
+  return higheNumber * 1 * word.length;
+};
+const arr = [
+  6, 5, 7, 3, 6, 7, 3, 4, 4, 2, 3, 7, 1, 3, 7, 4, 6, 1, 2, 4, 3, 3, 1, 1, 3, 5,
+];
+const resulted = designerPdfViewer(arr, "nzbkkfhwplj");
+checkResult(70, resulted);

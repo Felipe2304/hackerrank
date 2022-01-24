41f3aff9d8a54c2cb4ec4771c7e76596ac7bd4d6
Felipe martins <felipeaxandre936@gmail.com>
2022-01-24 10:33:10 -0300

    angry_professor

---
+++ angry_professor.js
@@ -0,0 +1,21 @@
+const checkResult = (a, b) => {
+  if (a === b) console.log("resposta correta", b);
+  else console.log("resposta errada", b, "deveria ser", a);
+};
+
+const angryProfessor = (k, a) => {
+  let counter = 0;
+  a.forEach((currentValue) => {
+    if (currentValue <= 0) counter++;
+  });
+  
+  if (counter >= k) {
+    return "NO";
+  } else {
+    return "YES";
+  }
+};
+
+const arr = [-1, -3, 4, 2];
+const resulted = angryProfessor(2, arr);
+checkResult('YES',resulted)

e1178fc34c1e1b41ca78b008075e484722a405fc
Felipe martins <felipeaxandre936@gmail.com>
2022-02-05 14:56:33 -0300

    apple_and_orange

--- cut_the_sticks.js
+++ cut_the_sticks.js
@@ -1,1 +1,28 @@
+const checkResult = (a, b) => {
+  if (a === b) console.log("resposta correta", b);
+  else console.log("resposta errada", b, "deveria ser", a);
+};
+const cutTheSticks = (arr) => {
+  const cut = (stickes) => {
+    const minValue = Math.min(...stickes);
+    const newList = stickes.reduce((acc, value) => {
+      const result = value - minValue;
+      if (result > 0) return [...acc, result];
+      return acc;
+    }, []);
+    return newList;
+  };
+  const quantities = [arr.length];
+  let rodeMeuLoop = true;
+  let newArr = arr;
+  while (rodeMeuLoop) {
+    newArr = cut(newArr);
+    if (newArr.length < 1) break;
+    quantities.push(newArr.length);
+  }
+  return quantities;
+};
+const arr = [8, 8, 14, 10, 3, 5, 14, 12];
 
+const result = cutTheSticks(arr);
+console.log("resultado: ", result);
--- desafio.js
+++
@@ -1,5 +0,0 @@
-let teste = 123
-teste = teste.toString().split('').reverse().join('')
-console.log(parseInt(teste))
-
-
